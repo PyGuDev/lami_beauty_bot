d0e884d08fc4c6b92cd562c1c4f18d52e3176d63
PyGuDev <gubaev.andreb@gmail.com>
2022-03-08 01:03:46 +0500

    fix type OWNER

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-OWNER = os.getenv('OWNER')
+OWNER = int(os.getenv('OWNER'))
 DB_DSN = os.getenv('DATABASE_URL')
