32836d3444ad86c8c4c98ba883ec9070874c3f6e
PyGuDev <gubaev.andreb@gmail.com>
2022-03-13 21:45:35 +0500

    add variable BASE_DIR and TEST_DB_DSN to config

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 
 load_dotenv('.env')
 
+BASE_DIR = Path(__file__).resolve().parent
+
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 OWNER = int(os.getenv('OWNER'))
 DB_DSN = os.getenv('DATABASE_URL')
+TEST_DB_DSN = os.getenv('TEST_DATABASE_URL')
