9b2d7621fa7e84023fda0683b9f2ae1975e47555
PyGuDev <gubaev.andreb@gmail.com>
2022-03-14 19:58:14 +0500

    add test method get records

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,7 +1,7 @@
 import logging
 
 from services.db import DB, DBManager
-from services.models import User
+from services.models import User, Record, RecordWithUser
 from config import DB_DSN, TEST_DB_DSN
 from tests import querys
 from tests.utilits import loads_fixtures
@@ -11,11 +11,11 @@ logging.basicConfig(format='%(asctime)s %(clientip)-15s %(user)-8s %(message)s')
 
 
 class CreateTestDataBase:
-    db = None
+    connected_db = None
     fixtures_data = None
 
     def setup_class(cls):
-        cls.db = DB(TEST_DB_DSN)
+        cls.connected_db = DB(TEST_DB_DSN)
         cls.fixtures_data = loads_fixtures()
         cls._write_test_data()
 
@@ -24,19 +24,19 @@ class CreateTestDataBase:
 
     @classmethod
     def _write_test_data(cls):
-        with cls.db.cursor as cur:
+        with cls.connected_db.cursor as cur:
             if cls.fixtures_data.get('users'):
                 cur.executemany(querys.INSERT_USER, cls.fixtures_data.get('users'))
             if cls.fixtures_data.get('records'):
                 cur.executemany(querys.INSERT_RECORD, cls.fixtures_data.get('records'))
-            cls.db.commit()
+            cls.connected_db.commit()
 
     @classmethod
     def _delete_test_data(cls):
-        with cls.db.cursor as cur:
-            cur.execute("delete from users")
+        with cls.connected_db.cursor as cur:
             cur.execute("delete from records")
-            cls.db.commit()
+            cur.execute("delete from users")
+            cls.connected_db.commit()
 
 
 class TestDB:
@@ -56,10 +56,18 @@ class TestDbManager(CreateTestDataBase):
         for data in data_users:
             user = User(**data)
             result = self.manager.get_user(user)
-            new_user = User(**result)
-            new_user.id = None
-            assert user == new_user
+            user_from_db = User(**result)
+            user_from_db.id = None
+            assert user == user_from_db
 
-    def test_record(self):
-        pass
+    def test_get_records(self):
+        data = self.manager.get_records()
+        for row in data:
+            assert RecordWithUser(**row)
 
+    def test_get_records_by_user(self):
+        data = self.manager.get_record_by_user_id(1212)
+        assert Record(**data)
+
+        data = self.manager.get_record_by_user_id(1212, add_user_data=True)
+        assert RecordWithUser(**data)
